03cfacc29e25fef808921c2684db4fd5c4cadbd5
piotrpdev <piotrpdev@gmail.com>
2024-10-22 14:26:13 +0100

    New button for upcoming movies page.

---
+++ movies/src/components/cardIcons/addToToWatchList.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const AddToToWatchListIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const handleAddToFavorites = (e) => {
+    e.preventDefault();
+    // context.addToFavorites(movie);
+  };
+
+  return (
+    <IconButton aria-label="add to to watch list" onClick={handleAddToFavorites}>
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToToWatchListIcon;
--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToToWatchListIcon from '../components/cardIcons/addToToWatchList'
 
 const UpcomingMovesPage = (props) => {
 
@@ -28,7 +28,7 @@ const UpcomingMovesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <AddToToWatchListIcon movie={movie} />
       }}
     />
   );
