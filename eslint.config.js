a4d1d34052e620315bf959d04d2e9de2c4d9f3ee
Piotr Płaczek <piotrpdev@gmail.com>
2024-11-21 15:18:14 +0000

    style: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,10 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import unusedImports from "eslint-plugin-unused-imports"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
+  eslintPluginPrettierRecommended,
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
